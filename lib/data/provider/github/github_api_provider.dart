afbdb397939ab0b9890c9cf29cc832037b5bce1a
Reuben Paterno <reuben.paterno@gmail.com>
2023-04-04 13:23:41 +0800

    Add repository and provider abstract and impl

---
+++ lib/data/provider/github/github_api_provider.dart
@@ -0,0 +1,44 @@
+import 'package:dio/dio.dart';
+import 'package:github_coding_test/data/model/github_repository_model.dart';
+import 'package:github_coding_test/data/model/github_search_schema.dart';
+import 'package:github_coding_test/data/model/repository_details.dart';
+import 'package:github_coding_test/data/provider/github/github_provider.dart';
+
+class GithubApiProvider implements GithubProvider {
+  final Dio dio;
+
+  GithubApiProvider(this.dio);
+
+  @override
+  Future<RepositoryDetail> getRepository(
+      {required String owner, required String repoName}) async {
+    final response = await dio.get('/repos/$owner/$repoName');
+
+    if (response.statusCode != null &&
+        response.statusCode! >= 200 &&
+        response.statusCode! < 300) {
+      final repository = RepositoryDetail.fromJson(response.data);
+      return repository;
+    } else {
+      throw Exception();
+    }
+  }
+
+  @override
+  Future<Iterable<GithubRepositoryModel>> searchRepositories(
+      String query) async {
+    final Map<String, dynamic> queryParams = {'query': query};
+
+    final response =
+        await dio.get('/search/code', queryParameters: queryParams);
+
+    if (response.statusCode != null &&
+        response.statusCode! >= 200 &&
+        response.statusCode! < 300) {
+      final githubSchema = GithubSearchSchema.fromJson(response.data);
+      return githubSchema.items;
+    } else {
+      throw Exception();
+    }
+  }
+}
